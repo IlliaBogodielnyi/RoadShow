f3bab60538cc56eae898493f61fb3fb79ccb786c
PC <PC@DESKTOP-S5TAQ6L>
2025-03-27 10:15:12 -0400

    feat: Add db data scaffold

--- Database/Queries/roadshow_db.sql
+++ Database/Queries/roadshow_db.sql
@@ -26,8 +26,18 @@ CREATE TABLE [dbo].[ProductLine]
         (
                 [ProductLineID] [bigint] IDENTITY (1, 1) NOT FOR REPLICATION NOT NULL,
                 [Name] [nvarchar](50) NOT NULL,
+				[VendorID] [bigint],
                 CONSTRAINT [PK_ProductLine_ProductLineID] PRIMARY KEY CLUSTERED ( [ProductLineID] ASC )
 ) ON [PRIMARY];
+GO
+
+ALTER TABLE [dbo].[ProductLine] WITH CHECK ADD 
+	CONSTRAINT [FK_ProductLine_Vendor_VendorID] FOREIGN KEY
+	(
+		[VendorID]
+	) REFERENCES [dbo].[Vendor] (
+		[VendorID]
+	); 
 GO
 
 /****** Object:  Table [dbo].[Product] *****/
@@ -120,6 +130,52 @@ ALTER TABLE [dbo].[ProductCriteria] WITH CHECK ADD
 	) REFERENCES [dbo].[Criteria] (
 		[CriteriaID]
 	);
+GO
+
+/****** inserts ******/
+
+USE [roadshow_db]
+GO
+
+INSERT INTO [dbo].[Vendor]
+           ([Name])
+     VALUES
+           ('Microsoft'),
+		   ('Amazon'),
+		   ('Google'),
+		   ('MongoDB Inc')
+
+GO
+
+USE [roadshow_db]
+GO
+
+INSERT INTO [dbo].[ProductLine]
+           ([Name]
+           ,[VendorID])
+     VALUES
+           ('Azure', 1),
+		   ('Amazon Web Services', 2),
+		   ('Google Cloud Platform', 3)
+GO
+
+USE [roadshow_db]
+GO
+
+INSERT INTO [dbo].[Product]
+           ([ProductLineID]
+           ,[VendorID]
+           ,[Name]
+           ,[ShortDescription])
+     VALUES
+        (1,1,'Cosmos DB','Distributed, multi-model database service offered by Microsoft. It is designed to provide high availability, scalability, and low-latency access to data for modern applications. '),
+		(2,2,'DynamoDB',' Managed NoSQL database service provided by Amazon Web Services. It supports key-value and document data structures and is designed to handle a wide range of applications requiring scalability and performance.'),
+		(3,3,'Cloud Firestore','Flexible, scalable NoSQL cloud database that can store and sync data for client- and server-side development'),
+		(3,3,'Cloud Spanner','Globally distributed relational database service that offers both strong consistency and horizontal scalability'),
+		(3,3,'Cloud Bigtable','NoSQL big data database service suitable for large analytical and operational workloads'),
+		(3,3,'Cloud Memorystore','Fully managed in-memory data store service built on popular open-source in-memory data stores like Redis and Memcached'),
+		(3,3,'Cloud SQL','Fully-managed relational database that supports SQL Server, PostgreSQL, and MySQL'),
+		(NULL,4,'MongoDB','MongoDB is a source-available, cross-platform, document-oriented database program. Classified as a NoSQL database product, MongoDB uses JSON-like documents with optional schemas. Released in February 2009 by 10gen, it supports features like sharding, replication, and ACID transactions.')
 GO
 
 
--- RoadShow.API/Program.cs
+++ RoadShow.API/Program.cs
@@ -1,11 +1,18 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.EntityFrameworkCore;
+using RoadShow.API.DAL;
 
-// Add services to the container.
+var builder = WebApplication.CreateBuilder(args);
+string connectionString = "Server=DESKTOP-S5TAQ6L\\SQLEXPRESS; Database=roadshow_db; TrustServerCertificate=True; Integrated Security=True";
+                          //"Server = localhost; Database=roadshow_db; User=sa; Password=1q @W3e4r5t;";
+                          // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<RoadshowDbContext>(options =>
+    options.UseSqlServer(connectionString));
 
 var app = builder.Build();
 
